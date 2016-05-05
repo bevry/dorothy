1ac908f5ca581093b8b4e6aef4c4d2a462bf69d9
Benjamin Lupton <b@lupton.cc>
2016-05-05 15:35:49 +0800

    rejig ubuntu install

--- .userrc.sh
+++ .userrc.sh
@@ -196,15 +196,15 @@ elif [[ "$OS" = "Linux" ]]; then
 		fc-cache -f -v
 		cd ~
 	}
-	alias aptinstall='sudo apt-get update && sudo apt-get install curl build-essential openssl libssl-dev git python ruby httpie vim'
+	alias aptinstall='sudo apt-get install curl build-essential openssl libssl-dev git python ruby httpie vim'
 	alias exposeinstall='sudo apt-get install compiz compizconfig-settings-manager compiz-plugins-extra compiz-plugins-main compiz-plugins'
 	alias solarizedinstall='cd ~ && git clone git://github.com/sigurdga/gnome-terminal-colors-solarized.git && cd gnome-terminal-colors-solarized && chmod +x install.sh && cd ~ && rm -Rf gnome-terminal-colors-solarized'
 	alias atominstall='sudo add-apt-repository ppa:webupd8team/atom && sudo apt-get update && sudo apt-get install atom && apminstall'
-	alias shellinstall='sudo apt-get update && sudo apt-get install libnotify-bin libgnome-keyring-dev && atominstall'
-	alias install='setupgit && aptinstall && shellinstall && fontinstall && nvminstall && npminstall'
+	alias shellinstall='sudo apt-get update && sudo apt-get install libnotify-bin libgnome-keyring-dev'
+	alias install='setupgit && updateinstall && aptinstall && shellinstall && fontinstall && atominstall && nvminstall && npminstall && cleaninstall'
 	alias updateinstall='sudo apt-get update && sudo apt-get upgrade'
 	alias cleaninstall='sudo apt-get clean && sudo apt-get autoremove'
-	alias removeinstall='sudo apt-get remove --purge libreoffice* rhythmbox thunderbird shotwell gnome-mahjongg gnomine gnome-sudoku gnome-mines aisleriot imagemagick'
+	alias removeinstall='sudo apt-get remove --purge libreoffice* rhythmbox thunderbird shotwell gnome-mahjongg gnomine gnome-sudoku gnome-mines aisleriot imagemagick && cleaninstall'
 
 	# System
 	alias resetfirefox="rm ~/.mozilla/firefox/*.default/.parentlock"
