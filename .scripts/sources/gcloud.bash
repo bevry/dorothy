0f03a48efdfe48b4b53bdfefb526dadc431fbcb1
Benjamin Lupton <b@lupton.cc>
2017-04-10 14:18:28 +0800

    kill gcloud for now as not sure how it works

--- .scripts/sources/gcloud.bash
+++ .scripts/sources/gcloud.bash
@@ -2,10 +2,10 @@
 
 # Google Cloud SDK
 # https://cloud.google.com/functions/docs/quickstart
-# brew install Caskroom/cask/google-cloud-sdk
+# brew cask install google-cloud-sdk
 # gcloud components install alpha
 
 # shellcheck disable=SC1090
-source "$HOME/Applications/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+# source "$HOME/Applications/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
 # shellcheck disable=SC1090
-source "$HOME/Applications/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+# source "$HOME/Applications/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
