0b746f4ff57a2b65286182589f95ade34c52993f
Benjamin Lupton <b@lupton.cc>
2017-06-09 12:52:04 +0800

    in bash, set instead of add if original var is empty

--- .scripts/sources/var.sh
+++ .scripts/sources/var.sh
@@ -7,15 +7,19 @@ function var_set {
 function var_add {
 	local exists="no"
 	local X="${!1}"
-	local Y="${X//:/\\n}"
-	while read -r line; do
-		if test "$line" = "$2"; then
-			exists="yes"
-			break
-		fi
-	done < <(echo -e "$Y")
+	if test -z "$X"; then
+		export $1="$2"
+	else
+		local Y="${X//:/\\n}"
+		while read -r line; do
+			if test "$line" = "$2"; then
+				exists="yes"
+				break
+			fi
+		done < <(echo -e "$Y")
 
-	if test "$exists" = "no"; then
-		export $1="$2":"${!1}"
+		if test "$exists" = "no"; then
+			export $1="$2":"${!1}"
+		fi
 	fi
 }
