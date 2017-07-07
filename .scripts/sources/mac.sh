8bf8d6409dbd5dd9704dcf2d5b49e81e3d970b30
Benjamin Lupton <b@lupton.cc>
2017-07-07 16:38:41 +0800

    remove custom homebrew cask room config
    turns out it is just too much effort to wrestle with
    
    they can get 777 permissions, so other users can access them
    
    plus you get a deprecation notice now

--- .scripts/sources/mac.sh
+++ .scripts/sources/mac.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env sh
 
-# https://github.com/caskroom/homebrew-cask/blob/master/USAGE.md#options
-# uses env to make sure that HOMEBREW_CASK_OPTS is set regardless of shell
-# as doing a set via fish does not expose it in brew which is bash
-export BREW_APPDIR="$HOME/Applications"
-export BREW_CASKROOM="$HOME/.cache/Caskroom"
-alias brew="env HOMEBREW_CASK_OPTS='--appdir=$BREW_APPDIR --caskroom=$BREW_CASKROOM' brew"
-
 # Mac specific aliases
 alias md5sum='md5 -r'
 alias edithosts='sudo edit /etc/hosts'
