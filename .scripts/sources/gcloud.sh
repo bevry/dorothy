e74b81119d59c93541689225016f63f8998d7484
Benjamin Lupton <b@lupton.cc>
2017-07-07 16:54:58 +0800

    update gloud script for default homebrew path

--- .scripts/sources/gcloud.sh
+++ .scripts/sources/gcloud.sh
@@ -6,17 +6,23 @@
 # gcloud components install alpha
 # gcloud init
 
-if is_dir "$BREW_CASKROOM/google-cloud-sdk"; then
+if is_string "$BREW_CASKROOM"; then
+	GDIR="$BREW_CASKROOM/google-cloud-sdk"
+else
+	GDIR="$(brew --prefix)/Caskroom/google-cloud-sdk"
+fi
+
+if is_dir "$GDIR"; then
 	if is_bash; then
 		# shellcheck disable=SC1090
-		source "$BREW_CASKROOM/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+		source "$GDIR/latest/google-cloud-sdk/path.bash.inc"
 		# shellcheck disable=SC1090
-		source "$BREW_CASKROOM/google-cloud-sdk/latest/google-cloud-sdk/completion.bash.inc"
+		source "$GDIR/latest/google-cloud-sdk/completion.bash.inc"
 
 	elif is_zsh; then
 		# shellcheck disable=SC1090
-		source "$BREW_CASKROOM/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+		source "$GDIR/latest/google-cloud-sdk/path.zsh.inc"
 		# shellcheck disable=SC1090
-		source "$BREW_CASKROOM/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+		source "$GDIR/latest/google-cloud-sdk/completion.zsh.inc"
 	fi
 fi
