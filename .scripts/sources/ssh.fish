8552986433b4bca1a6e6297c919435c85d8d951f
Benjamin Lupton <b@lupton.cc>
2017-07-15 14:31:07 +0800

    update scripts for recent changes

--- .scripts/sources/ssh.fish
+++ .scripts/sources/ssh.fish
@@ -8,4 +8,5 @@ end
 function addsshkeys
 	eval (ssh-agent -c)
 	ssh-add -A
+	echo 'If your keys were not added, you may need to re-add them to the keychain via: addsshkey <name>'
 end
--- .scripts/sources/ssh.sh
+++ .scripts/sources/ssh.sh
@@ -8,4 +8,5 @@ function addsshkey {
 function addsshkeys {
 	eval "$(ssh-agent -s)"
 	ssh-add -A
+	echo 'If your keys were not added, you may need to re-add them to the keychain via: addsshkey <name>'
 }
