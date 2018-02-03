64d4e0dcf3abc45c0eed4a16bfc98ec9e7ddd4d4
Benjamin Lupton <b@lupton.cc>
2018-02-03 10:27:03 +0800

    secure functions working and more comprehensive

--- .scripts/sources/secure.sh
+++ .scripts/sources/secure.sh
@@ -2,9 +2,15 @@
 
 function secure {
 	if confirm "[y] to delete all history, [n] to delete some"; then
+		echo 'deleting all'
 		history -c
 	else
-		history delete --contains "http POST"
-		history delete --contains "http -f POST"
+		echo 'deleting some'
+		echo 'all' | history delete --contains "http POST"
+		echo 'all' | history delete --contains "http -f POST"
+		echo 'all' | history delete --contains "vault"
+		echo 'all' | history delete --contains "key"
+		echo 'all' | history delete --contains "token"
 	fi
+	echo 'deleted'
 }
