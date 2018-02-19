83b6de6651148db5b767d16ab45c0580f2f6dc7e
Benjamin Lupton <b@lupton.cc>
2018-02-19 14:15:28 +0800

    ssh: added startssh for creating a single ssh-agent

--- .scripts/sources/ssh.fish
+++ .scripts/sources/ssh.fish
@@ -1,12 +1,19 @@
 #!/usr/bin/env fish
 
+function startssh
+	# http://rabexc.org/posts/pitfalls-of-ssh-agents
+	if test -z "$SSH_AUTH_SOCK"
+		eval (ssh-agent -c)
+	end
+end
+
 function addsshkey
-	eval (ssh-agent -c)
+	startssh
 	ssh-add -K "$HOME/.ssh/$argv[1]"
 end
 
 function addsshkeys
-	eval (ssh-agent -c)
+	startssh
 	ssh-add -A
 	echo 'If your keys were not added, you may need to re-add them to the keychain via: addsshkey <name>'
 end
--- .scripts/sources/ssh.sh
+++ .scripts/sources/ssh.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
+function startssh {
+	# http://rabexc.org/posts/pitfalls-of-ssh-agents
+	if test -z "$SSH_AUTH_SOCK"; then
+		eval "$(ssh-agent -s)"
+	fi
+}
+
 function addsshkey {
-	eval "$(ssh-agent -s)"
+	startssh
 	ssh-add -K "$HOME/.ssh/$1"
 }
 
 function addsshkeys {
-	eval "$(ssh-agent -s)"
+	startssh
 	ssh-add -A
 	echo 'If your keys were not added, you may need to re-add them to the keychain via: addsshkey <name>'
 }
