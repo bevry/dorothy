6a18ca447d21dc8d88eb08dfa99ab5c70c6bf698
Benjamin Lupton <b@lupton.cc>
2018-09-26 16:07:25 +1000

    sources: array: add my own in-array function
    as I don’t know how element-in works

--- .scripts/sources/array.bash
+++ .scripts/sources/array.bash
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
 # https://stackoverflow.com/a/8574392/130638
-function element_in {
+# element-in needle haystack
+function element-in {
 	local e match="$1"
 	shift
 	for e; do [[ "$e" == "$match" ]] && return 0; done
 	return 1
+}
+
+# in-array needle haystack
+function in-array {
+	needle="$1"
+	haystack=("$2[@]")
+	for item in "${haystack[@]}"; do
+		if test "$needle" = "$item"; then
+			return 1
+		fi
+	done
+	return 0
 }
