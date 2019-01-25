4f9f49655e31717db67279a76c57cb8f4e202b74
Benjamin Lupton <b@lupton.cc>
2019-01-26 04:16:30 +1100

    nullgob, globstar, extglob: add descriptions

--- .scripts/sources/extglob.bash
+++ .scripts/sources/extglob.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+# extglob: If set, the extended pattern matching features described above (see Pattern Matching) are enabled.
+
 if [[ "$BASH_VERSION" = "4."* || "$BASH_VERSION" = "5."* ]]; then
 	shopt -s extglob
 else
