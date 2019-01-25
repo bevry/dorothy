9d0bb778af9542dc40890a1a8263d09b6ed28913
Benjamin Lupton <b@lupton.cc>
2019-01-26 04:16:30 +1100

    nullgob, globstar, extglob: add descriptions

--- .scripts/sources/globstar.bash
+++ .scripts/sources/globstar.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+# globstar: If set, the pattern ‘**’ used in a filename expansion context will match all files and zero or more directories and subdirectories. If the pattern is followed by a ‘/’, only directories and subdirectories match.
+
 if [[ "$BASH_VERSION" = "4."* || "$BASH_VERSION" = "5."* ]]; then
 	shopt -s globstar
 else
--- .scripts/sources/nullglob.bash
+++ .scripts/sources/nullglob.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
+# nullglob: If set, Bash allows filename patterns which match no files to expand to a null string, rather than themselves.
+
 if [[ "$BASH_VERSION" = "4."* || "$BASH_VERSION" = "5."* ]]; then
 	shopt -s nullglob
 else
