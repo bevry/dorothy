6fd647cea5842ee6bb8903dfbec81fc9f5526361
Benjamin Lupton <b@lupton.cc>
2019-01-26 04:17:03 +1100

    secure: add more things to clear in history
    also alpha sorted

--- .scripts/sources/functions.fish
+++ .scripts/sources/functions.fish
@@ -10,17 +10,18 @@ function secure_history
 		history -c
 	else
 		echo 'deleting some'
-		echo 'all' | history delete --contains "http POST"
-		echo 'all' | history delete --contains "http -f POST"
-		echo 'all' | history delete --contains "vault"
-		echo 'all' | history delete --contains "key"
-		echo 'all' | history delete --contains "token"
 		echo 'all' | history delete --contains "auth"
-		echo 'all' | history delete --contains "env"
-		echo 'all' | history delete --contains "session"
 		echo 'all' | history delete --contains "cookie"
-		echo 'all' | history delete --contains "secret"
+		echo 'all' | history delete --contains "env"
+		echo 'all' | history delete --contains "http -f POST"
+		echo 'all' | history delete --contains "http POST"
+		echo 'all' | history delete --contains "key"
 		echo 'all' | history delete --contains "op "
+		echo 'all' | history delete --contains "secret"
+		echo 'all' | history delete --contains "session"
+		echo 'all' | history delete --contains "token"
+		echo 'all' | history delete --contains "vault"
+		echo 'all' | history delete --contains "youtube-dl"
 	end
 	echo 'deleted'
 end
