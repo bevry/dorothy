247047c54984ffcc8f594c0c0b77c2b067e178b5
Benjamin Lupton <b@lupton.cc>
2019-05-07 11:43:02 +1000

    balupton: source: update for latest packages

--- .scripts/users/balupton/source.sh
+++ .scripts/users/balupton/source.sh
@@ -5,3 +5,6 @@ export SHELLCHECK_OPTS="-e SC2096 -e SC1090 -e SC1091 -e SC1071"
 export USER_SHELL="fish"
 export GIT_PROTOCOL="ssh"
 export KRYPTON_GPG="no"
+
+alias textile="$HOME/Projects/textile/go-textile/dist/textile"
+alias go-open="open -a /Applications/GoLand.app ."
