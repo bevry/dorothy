d0b381a398ad3fbbc14b9df0de26cd0fc85e1291
Benjamin Lupton <b@lupton.cc>
2020-06-04 13:38:25 +1000

    complete flattening

--- init.fish
+++ init.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
-set BDIR (dirname (status -f))
+set -xg BDIR (dirname (status -f))
 source "$BDIR/sources/essentials.fish"
 source "$BDIR/sources/extras.fish"
--- init.sh
+++ init.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env sh
 
-export BDIR; BDIR="$(realpath "$0")"
+if test -z "${BDIR:-}"; then
+	# https://stackoverflow.com/a/246128
+	# https://stackoverflow.com/a/14728194
+	export BDIR; BDIR="$(dirname "${BASH_SOURCE:-$0}")"
+fi
+
 . "$BDIR/sources/essentials.sh"
 . "$BDIR/sources/extras.sh"
