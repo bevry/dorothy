93516ef251a3e98bdbc56c33495d20d99d8bad96
Benjamin Lupton <b@lupton.cc>
2020-09-24 07:27:26 +1000

    balupton: skip KRYPTON_GPG as GPG_SIGNING_KEY is now what determines its usage

--- users/balupton/source.sh
+++ users/balupton/source.sh
@@ -7,7 +7,6 @@ export THEME='baltheme'
 export SHELLCHECK_OPTS="-e SC2096 -e SC1090 -e SC1091 -e SC1071"
 export USER_SHELL="fish"
 export GIT_PROTOCOL="ssh"
-export KRYPTON_GPG="no"
 export GITHUB_API="https://bevry.me/api/github"
 
 alias go-open="open -a /Applications/GoLand.app ."
