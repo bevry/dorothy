9388dec487c6bba8693329418d9584667c747d56
Benjamin Lupton <b@lupton.cc>
2021-07-06 08:56:32 +0800

    theme.sh: source it only when needed
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- sources/theme.sh
+++ sources/theme.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 if test "${DOROTHY_THEME-}" = 'oz'; then
-	. "$DOROTHY/themes/oz"
 	if is-string "${BASH_VERSION-}"; then
 		export PROMPT_COMMAND="oztheme bash $?"
 	elif is-string "${ZSH_VERSION-}"; then
+		. "$DOROTHY/themes/oz"
 		precmd () {
 			oztheme zsh "$?"
 		}
