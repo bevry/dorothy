5eaefd27f8b29f5a12ef81b76c320aff44d1edc7
Benjamin Lupton <b@lupton.cc>
2021-07-06 06:08:01 +0000

    improvements to trial mode
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- sources/theme.sh
+++ sources/theme.sh
@@ -1,23 +1,38 @@
 #!/usr/bin/env sh
 
-if test "${DOROTHY_THEME-}" = 'oz'; then
-	if is-string "${BASH_VERSION-}"; then
-		export PROMPT_COMMAND="oztheme bash $?"
-	elif is-string "${ZSH_VERSION-}"; then
-		. "$DOROTHY/themes/oz"
-		precmd () {
-			oztheme zsh "$?"
-		}
+if ! test -z "${DOROTHY_THEME-}" -o "${DOROTHY_THEME}" = 'system'; then
+	if test "${DOROTHY_THEME}" = 'oz'; then
+		if test -n "${BASH_VERSION-}"; then
+			. "$DOROTHY/themes/oz"
+			export PROMPT_COMMAND="oztheme bash $?"
+		elif test -n "${ZSH_VERSION-}"; then
+			. "$DOROTHY/themes/oz"
+			precmd () {
+				oztheme zsh "$?"
+			}
+		else
+			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+		fi
+	elif test "${DOROTHY_THEME}" = 'starship'; then
+		if test -n "${BASH_VERSION-}"; then
+			eval "$(starship init bash)"
+		elif test -n "${ZSH_VERSION-}"; then
+			eval "$(starship init zsh)"
+		else
+			# https://starship.rs/guide/#🚀-installation
+			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+		fi
+	elif test "${DOROTHY_THEME}" = 'trial'; then
+		if test -n "${BASH_VERSION-}"; then
+			export PROMPT_COMMAND="echo -n 'DorothyTrial> '"
+		elif test -n "${ZSH_VERSION-}"; then
+			precmd () {
+				echo -n 'DorothyTrial> '
+			}
+		else
+			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+		fi
 	else
-		stderr echo 'dorothy does not yet support the oz theme on this shell'
-	fi
-elif test "${DOROTHY_THEME-}" = 'starship'; then
-	if is-string "${BASH_VERSION-}"; then
-		eval "$(starship init bash)"
-	elif is-string "${ZSH_VERSION-}"; then
-		eval "$(starship init zsh)"
-	else
-		# https://starship.rs/guide/#🚀-installation
-		stderr echo 'dorothy does not yet support the starship theme on this shell'
+		stderr echo "dorothy does not understand the theme [$DOROTHY_THEME]"
 	fi
 fi
