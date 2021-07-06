624d404f399e8d939a4d586b635c7586163d044d
Benjamin Lupton <b@lupton.cc>
2021-07-06 06:08:01 +0000

    improvements to trial mode
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- sources/theme.fish
+++ sources/theme.fish
@@ -1,9 +1,17 @@
 #!/usr/bin/env fish
 
-if test "$DOROTHY_THEME" = 'oz'
-	function fish_prompt
-		"$DOROTHY/themes/oz" fish "$status"
+if ! test -z "$DOROTHY_THEME" -o "$DOROTHY_THEME"  = 'system'
+	if test "$DOROTHY_THEME" = 'oz'
+		function fish_prompt
+			"$DOROTHY/themes/oz" fish "$status"
+		end
+	else if test "$DOROTHY_THEME" = 'starship'
+		starship init fish | source
+	else if test "$DOROTHY_THEME" = 'trial'
+		function fish_prompt
+			echo -n 'DorothyTrial> '
+		end
+	else
+		stderr echo "dorothy does not understand the theme [$DOROTHY_THEME]"
 	end
-else if test "$DOROTHY_THEME" = 'starship'
-	starship init fish | source
 end
