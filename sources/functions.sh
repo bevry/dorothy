af3d2c368df8477780ade9ca86e38333a533f780
Benjamin Lupton <b@lupton.cc>
2021-07-14 22:10:51 +0800

    functions; fix `secure_history`
    turns out bash and zsh do not support selective deletes

--- sources/functions.sh
+++ sources/functions.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env sh
 
 secure_history () {
-	what="${1:-"$(choose all some)"}"
-	if test "$what" = "all"; then
-		echo 'deleting all'
+	what="${1:-"$(choose 'delete only the known risks' 'erase your entire history')"}"
+	if test "$what" = 'erase your entire history'; then
 		history -c
+		echo 'Erased everything.'
 	else
-		echo 'deleting some'
-		echo 'all' | history delete --contains "auth"
-		echo 'all' | history delete --contains "cookie"
-		echo 'all' | history delete --contains "env"
-		echo 'all' | history delete --contains "http -f POST"
-		echo 'all' | history delete --contains "http POST"
-		echo 'all' | history delete --contains "key"
-		echo 'all' | history delete --contains "op "
-		echo 'all' | history delete --contains "secret"
-		echo 'all' | history delete --contains "session"
-		echo 'all' | history delete --contains "token"
-		echo 'all' | history delete --contains "twurl"
-		echo 'all' | history delete --contains "vault"
-		echo 'all' | history delete --contains "youtube-dl"
+		stderr echo 'Erasing only known risks is not supported in shells that are not Fish.'
+		return 1
 	fi
-	echo 'deleted'
 }
