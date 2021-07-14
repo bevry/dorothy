4527e52b33934ca01a64b0f8d1cba2dd231ba291
Benjamin Lupton <b@lupton.cc>
2021-07-14 22:10:51 +0800

    functions; fix `secure_history`
    turns out bash and zsh do not support selective deletes

--- sources/functions.fish
+++ sources/functions.fish
@@ -3,26 +3,25 @@
 function secure_history
 	set what $argv[1]
 	if is-empty-string "$what"
-		set what (choose all some)
+		set what (choose 'delete only the known risks' 'erase your entire history')
 	end
-	if test "what" = "all"
-		echo 'deleting all'
+	if test "$what" = 'erase your entire history'; then
 		history -c
+		echo 'Erased everything.'
 	else
-		echo 'deleting some'
-		echo 'all' | history delete --contains "auth"
-		echo 'all' | history delete --contains "cookie"
-		echo 'all' | history delete --contains "env"
-		echo 'all' | history delete --contains "http -f POST"
-		echo 'all' | history delete --contains "http POST"
-		echo 'all' | history delete --contains "key"
-		echo 'all' | history delete --contains "op "
-		echo 'all' | history delete --contains "secret"
-		echo 'all' | history delete --contains "session"
-		echo 'all' | history delete --contains "token"
-		echo 'all' | history delete --contains "twurl"
-		echo 'all' | history delete --contains "vault"
-		echo 'all' | history delete --contains "youtube-dl"
+		echo 'all' | history delete --contains 'auth'
+		echo 'all' | history delete --contains 'cookie'
+		echo 'all' | history delete --contains 'env'
+		echo 'all' | history delete --contains 'http -f POST'
+		echo 'all' | history delete --contains 'http POST'
+		echo 'all' | history delete --contains 'key'
+		echo 'all' | history delete --contains 'op '
+		echo 'all' | history delete --contains 'secret'
+		echo 'all' | history delete --contains 'session'
+		echo 'all' | history delete --contains 'token'
+		echo 'all' | history delete --contains 'twurl'
+		echo 'all' | history delete --contains 'vault'
+		echo 'all' | history delete --contains 'youtube-dl'
+		echo 'Erased known risks.'
 	end
-	echo 'deleted'
 end
