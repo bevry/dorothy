3e6f7a73007e01398745a3ebf80debb495866fc7
Benjamin Lupton <b@lupton.cc>
2021-07-29 17:05:14 +0800

    init.sh: avoid double load

--- init.sh
+++ init.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
-if test -z "${DOROTHY-}"; then
-	# https://stackoverflow.com/a/246128
-	# https://stackoverflow.com/a/14728194
-	export DOROTHY; DOROTHY="$(dirname "${BASH_SOURCE:-$0}")"
-fi
+# under some environments
+# only bashrc is loaded
+# only bash_profile is loaded
+# both bashrc and bash_profile is loaded
+# as such, avoid double-loads
+if test -z "${DOROTHY_INIT-}"; then
+	export DOROTHY_INIT='y'
 
-. "$DOROTHY/sources/essentials.sh"
-. "$DOROTHY/sources/extras.sh"
+	if test -z "${DOROTHY-}"; then
+		# https://stackoverflow.com/a/246128
+		# https://stackoverflow.com/a/14728194
+		export DOROTHY; DOROTHY="$(dirname "${BASH_SOURCE:-$0}")"
+	fi
+
+	. "$DOROTHY/sources/essentials.sh"
+	. "$DOROTHY/sources/extras.sh"
+fi
