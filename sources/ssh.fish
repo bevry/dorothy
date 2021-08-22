0d4c8ba8f5e3fc6dee438cd48286b248bd066b0c
Benjamin Lupton <b@lupton.cc>
2021-08-22 07:31:54 +0000

    sources/ssh: fix rsync, fix fish

--- sources/ssh.fish
+++ sources/ssh.fish
@@ -1,17 +1,24 @@
 #!/usr/bin/env fish
 
+# set --local fish_trace on
+set --global SSH_AUTH_SOCK
+set --global SSH_AGENT_PID
+
 # only work on environments that have an ssh-agent
 if command-exists ssh-agent
 	# start the ssh agent
 	if test -z "$SSH_AUTH_SOCK"
-		eval (ssh-agent -c)
+		eval (ssh-agent -c | sed -E 's/^setenv /set --global --export /' | sed -E 's/^echo /#echo /')
 		# ssh-add-all
 	end
 
 	# kill it when our cli ends
 	function finish
 		# killall ssh-agent
-		eval (ssh-agent -k)
+		eval (ssh-agent -k | sed -E 's/^unset /set --erase /' | sed -E 's/^echo /#echo /')
 	end
 	trap finish EXIT
 end
+
+# set --erase fish_trace
+
--- sources/ssh.sh
+++ sources/ssh.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env sh
 
+# silent is done to prevent rsync ssh failures
+# https://fixyacloud.wordpress.com/2020/01/26/protocol-version-mismatch-is-your-shell-clean/
+
 # only work on environments that have an ssh-agent
 if command-exists ssh-agent; then
 	# start the ssh agent
 	if is-empty-string "${SSH_AUTH_SOCK-}"; then
-		eval "$(ssh-agent -s)"
+		eval "$(ssh-agent -s)"  >/dev/null 2>&1
 		# ssh-add-all
 	fi
 
 	# kill it when our cli ends
 	finish () {
 		# killall ssh-agent
-		eval "$(ssh-agent -k)"
+		eval "$(ssh-agent -k)"  >/dev/null 2>&1
 	}
 	trap finish EXIT
 fi
