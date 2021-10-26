697b64ce8d18c22c39dac6ccd51f4ea4fed11582
Benjamin Lupton <b@lupton.cc>
2021-10-26 13:08:41 +0800

    fix HOME, USER env vars, and editor handling
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- sources/edit.fish
+++ sources/edit.fish
@@ -1,15 +1,15 @@
 #!/usr/bin/env fish
 
-eval (setup-editor-commands fish)
+eval (env -i DOROTHY="$DOROTHY" DOROTHY_USER_HOME="$DOROTHY_USER_HOME" USER="$USER" HOME="$HOME" PATH="$PATH" "$DOROTHY/commands/setup-editor-commands" fish)
 function edit
 	if is-ssh
-		if is-empty-string "$TERMINAL_EDITOR"
+		if test -z "$TERMINAL_EDITOR"
 			echo "TERMINAL_EDITOR must be configured"
 		else
 			eval "$TERMINAL_EDITOR" '"'$argv'"'
 		end
 	else
-		if is-empty-string "$GUI_EDITOR"
+		if test -z "$GUI_EDITOR"
 			echo "GUI_EDITOR must be configured"
 		else
 			eval "$GUI_EDITOR" '"'$argv'"'
--- sources/edit.sh
+++ sources/edit.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
-eval "$(setup-editor-commands)"
-edit () {
+eval "$(env -i DOROTHY="$DOROTHY" DOROTHY_USER_HOME="$DOROTHY_USER_HOME" USER="$USER" HOME="$HOME" PATH="$PATH" "$DOROTHY/commands/setup-editor-commands")"
+edit() {
 	if is-ssh; then
-		eval "${TERMINAL_EDITOR:?TERMINAL_EDITOR must be configured}" "$@"
+		eval "${TERMINAL_EDITOR:?"TERMINAL_EDITOR must be configured"}" "$@"
 	else
-		eval "${GUI_EDITOR:?GUI_EDITOR must be configured}" "$@"
+		eval "${GUI_EDITOR:?"GUI_EDITOR must be configured"}" "$@"
 	fi
 }
