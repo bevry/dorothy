a3e1a51b28e10dde7a9f4e82e51d39c5efbd2b88
Benjamin Lupton <b@lupton.cc>
2021-11-01 10:18:12 +0800

    env: flatpak xdg support, don't add duplicate paths
    Signed-off-by: Benjamin Lupton <b@lupton.cc>

--- sources/env.bash
+++ sources/env.bash
@@ -7,13 +7,13 @@
 # echos should suffix with ; otherwise fish will break
 
 # initial scanning of environment into a tuple]
-inherited=()  # [name, value], [name, value], ...
+inherited=() # [name, value], [name, value], ...
 while read -r line; do
 	# shellcheck disable=SC2066
-	for ((i=0; i<${#line}; i++)); do
+	for ((i = 0; i < ${#line}; i++)); do
 		if test "${line:i:1}" = '='; then
-			inherited+=("${line:0:i}")  # name
-			inherited+=("${line:i+1}")  # value
+			inherited+=("${line:0:i}") # name
+			inherited+=("${line:i+1}") # value
 			break
 		fi
 	done
@@ -28,24 +28,57 @@ function finish() {
 	fi
 
 	# success condition, echo var actions
-	local name value i
+	local name value i items_array items_string item item_last_index item_index item_existing
 	while read -r line; do
 		# parse line
 		name='' value=''
-
 		# shellcheck disable=SC2066
-		for ((i=0; i<${#line}; i++)); do
+		for ((i = 0; i < ${#line}; i++)); do
 			if test "${line:i:1}" = '='; then
-				name="${line:0:i}"   # name
-				value="${line:i+1}"  # value
+				name="${line:0:i}"  # name
+				value="${line:i+1}" # value
 				break
 			fi
 		done
 
+		# adjust
+		if [[ "$name" = *'PATH' ]] || [[ "$name" = *'DIRS' ]]; then
+			# cycle through each item in the path, removing duplicates and empties
+			items_array=()
+			items_string=''
+			item_last_index=0
+			# shellcheck disable=SC2066
+			for ((item_index = 0; item_index < ${#value}; item_index++)); do
+				if test "${value:item_index:1}" = ':'; then
+					item="${value:item_last_index:item_index-item_last_index}"
+					item_last_index="$((item_index + 1))"
+					# check if empty
+					if test -z "$item"; then
+						continue
+					fi
+					# check if duplicate
+					for item_existing in "${items_array[@]}"; do
+						if test "$item" = "$item_existing"; then
+							# is duplicate, skip
+							continue 2
+						fi
+					done
+					# add
+					items_array+=("$item")
+					if test -z "$items_string"; then
+						items_string="$item"
+					else
+						items_string="$items_string:$item"
+					fi
+				fi
+			done
+			value="$items_string"
+		fi
+
 		# find it in inherited, and check if it is the same if it is the same as inherited
-		for ((i=0; i<${#inherited[@]}; i+=2)); do
+		for ((i = 0; i < ${#inherited[@]}; i += 2)); do
 			if test "${inherited[$i]}" = "${name}"; then
-				if test "${inherited[$i+1]}" = "${value}"; then
+				if test "${inherited[$i + 1]}" = "${value}"; then
 					# is inherited, continue to next item
 					continue 2
 				fi
@@ -57,7 +90,7 @@ function finish() {
 		# 	echo "set --universal --erase $name;"
 		# fi
 		if test -z "$value"; then
-			# delete
+			# echo var action: delete
 			# shellcheck disable=SC2154
 			if test "$shell" = 'fish'; then
 				echo "set --universal --erase $name;"
@@ -65,17 +98,14 @@ function finish() {
 				echo "export $name='';"
 			fi
 		elif [[ "$name" = *'PATH' ]] || [[ "$name" = *'DIRS' ]]; then
-			# trim trailing nothing
-			separator_index="${#value}"
-			if test "${value:separator_index-1:1}" = ':'; then
-				value="${value:0:separator_index-1}"
-			fi
+			# echo var action: set path
 			if test "$shell" = 'fish'; then
 				echo "set --export --path $name '$value';"
 			else
 				echo "export $name='$value';"
 			fi
 		else
+			# echo var action: set
 			if test "$shell" = 'fish'; then
 				echo "set --export $name '$value';"
 			else
