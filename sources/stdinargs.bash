04c24f21b421436190a9fa04d73b192b2c167cef
Benjamin Lupton <b@lupton.cc>
2021-11-03 07:40:30 +0800

    echo-or-fail: use `stdinargs.bash` `help` function
    stdinargs: `help` function

--- sources/stdinargs.bash
+++ sources/stdinargs.bash
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-
 # calls the act function on each stdin line or argument
+
+# check for help argument
+if is-help "$@" && test "$(type -t help)" = 'function'; then
+	help && exit 22 # Invalid argument
+	exit "$?"
+fi
 
 # wait 0.1 seconds for stdin lines, and call act on each of them
 stdin="no"
@@ -14,7 +19,8 @@ if test "$stdin" = "no"; then
 	# if no args, and noact exists, call noact
 	if test "$#" -eq 0; then
 		# check if noact exists
-		if test "$(type -t noact)" = 'function' ; then
+		if test "$(type -t noact)" = 'function'; then
+
 			noact
 			exit "$?"
 		fi
@@ -27,7 +33,7 @@ if test "$stdin" = "no"; then
 fi
 
 # check if finish exists
-if test "$(type -t finish)" = 'function' ; then
+if test "$(type -t finish)" = 'function'; then
 	finish
 	exit "$?"
 fi
