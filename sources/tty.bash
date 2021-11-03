2a5a13109aa050fbb7dcae0984772027c5cecc39
Benjamin Lupton <b@lupton.cc>
2021-11-03 08:46:47 +0800

    ask, choose-menu, choose-option: remove `--main`
    now replaced by `env NO_ALT_TTY=yes` or `env ALT_TTY=no`
    
    First introduced in:
    
    https://github.com/bevry/dorothy/commit/6f841ad89c3e8b8e818fb379dcc746ed0a8a4ed2
    
    Closes:
    
    https://github.com/bevry/dorothy/issues/27

--- sources/tty.bash
+++ sources/tty.bash
@@ -4,24 +4,40 @@
 # These create a new TTY that can be cleared without affecting the prior TTY.
 # https://unix.stackexchange.com/a/668615/50703
 
-tty_start () {
-	tput smcup > /dev/tty
-	tput cup 0 0 > /dev/tty
+tty_start() {
+	tput smcup >/dev/tty
+	tput cup 0 0 >/dev/tty
 }
 
-tty_clear () {
-	tput clear > /dev/tty # also resets cursor to top
+tty_clear() {
+	tput clear >/dev/tty # also resets cursor to top
 }
 
-tty_finish () {
+tty_finish() {
 	# `tput rmcup` does not persist stderr, so for failure/stderr dumps, use `sleep 10` to ensure sterr is visible for long enough to be noticed before wiped.
-	tput rmcup > /dev/tty
+	tput rmcup >/dev/tty
 }
 
-tty_auto () {
+tty_auto() {
 	tty_start
 	trap tty_finish EXIT
 }
+
+# if alt tty is disabled however, then disable it
+if is-affirmative "${NO_ALT_TTY-}" || test "$(echo-exit-code is-affirmative "${ALT_TTY-}")" -eq 1; then
+	tty_start() {
+		return
+	}
+	tty_clear() {
+		return
+	}
+	tty_finish() {
+		return
+	}
+	tty_auto() {
+		return
+	}
+fi
 
 # -------------------------------------
 # The below methods are only useful if the y is below $LINES
@@ -29,28 +45,31 @@ tty_auto () {
 # As such, these are prone to failure, and you should use the earlier methods instead.
 # https://stackoverflow.com/a/69138082/130638
 
-tty_get_y_x () {
+tty_get_y_x() {
 	local y x
-	IFS='[;' read -srd R -p $'\e[6n' _ y x < /dev/tty
+	IFS='[;' read -srd R -p $'\e[6n' _ y x </dev/tty
 	echo "$y"
 	echo "$x"
 }
 
-tty_set_y_x () {
+tty_set_y_x() {
 	local y x
 	if test "$#" -eq 0; then
-		mapfile -t yx < <(tty_get_y_x); y="${yx[0]}"; x="${yx[1]}"
+		mapfile -t yx < <(tty_get_y_x)
+		y="${yx[0]}"
+		x="${yx[1]}"
 	else
-		y="${1-}"; x="${2-}"
+		y="${1-}"
+		x="${2-}"
 	fi
-	echo -en "\e[${y};${x}H" > /dev/tty
+	echo -en "\e[${y};${x}H" >/dev/tty
 }
 
-tty_erase_to_end () {
-	echo -en "\e[J" > /dev/tty
+tty_erase_to_end() {
+	echo -en "\e[J" >/dev/tty
 }
 
-tty_erase_from_y_x () {
+tty_erase_from_y_x() {
 	tty_set_y_x "${1-}" "${2-}"
 	tty_erase_to_end
 }
