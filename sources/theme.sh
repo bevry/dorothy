bevry/dorothy

commit 5aec180b7a9e5329b712165f8f184c0a38d4c778

fix environment and edit sources failing on fish login shells
This was due to ACTIVE_SHELL being used, yet that would be fish, so if a bash script sourced `environment.sh` or `edit.sh`, then fish would be used instead of the correct active shell.

As such:

1. `ACTIVE_SHELL` has been renamed to `ACTIVE_LOGIN_SHELL`, and moved to `shell.sh` and `shell.fish`.
2. Moved the detection code to `get_active_shell.sh`, and have `edit.sh` and `environment.sh` source it.

Other changes include:

- Consolidating completion code, into `completions.fish` and `completions.sh`.
- Cleaning active shell checks
- Cleaning `test ...` usage.

sources/theme.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env sh
 
-if ! test -z "${DOROTHY_THEME-}" -o "${DOROTHY_THEME}" = 'system'; then
-	if test "${DOROTHY_THEME}" = 'oz'; then
-		if test -n "${BASH_VERSION-}"; then
+if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
+	if test "$DOROTHY_THEME" = 'oz'; then
+		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			. "$DOROTHY/themes/oz"
 			export PROMPT_COMMAND="oztheme bash \$?"
-		elif test -n "${ZSH_VERSION-}"; then
+		elif test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			. "$DOROTHY/themes/oz"
 			precmd() {
 				# shellcheck disable=SC3043
 				local last_command_exit_status="$?"
-				if ! test -d "$DOROTHY"; then
+				if test ! -d "$DOROTHY"; then
 					echo 'DOROTHY has been moved, please re-open your shell'
 					return 1
 				fi
@@ -19,19 +19,19 @@ if ! test -z "${DOROTHY_THEME-}" -o "${DOROTHY_THEME}" = 'system'; then
 		else
 			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
 		fi
-	elif test "${DOROTHY_THEME}" = 'starship'; then
-		if test -n "${BASH_VERSION-}"; then
+	elif test "$DOROTHY_THEME" = 'starship'; then
+		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			eval "$(starship init bash)"
-		elif test -n "${ZSH_VERSION-}"; then
+		elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
 			eval "$(starship init zsh)"
 		else
 			# https://starship.rs/guide/#🚀-installation
 			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
 		fi
-	elif test "${DOROTHY_THEME}" = 'trial'; then
-		if test -n "${BASH_VERSION-}"; then
+	elif test "$DOROTHY_THEME" = 'trial'; then
+		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			export PROMPT_COMMAND="echo -n 'DorothyTrial> '"
-		elif test -n "${ZSH_VERSION-}"; then
+		elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
 			precmd() {
 				printf 'DorothyTrial> '
 			}

commands/android-dev
@@ -3,7 +3,7 @@ source "$DOROTHY/sources/strict.bash"
 
 # Android Simulator
 app=$(get-app "Android Studio.app")
-if is-dir "app"; then
+if test -d "app"; then
 	"$app/sdk/tools/emulator" -avd basic
 else
 	echo "Android Studio is not installed"

commands/concat-files
@@ -3,7 +3,7 @@ source "$DOROTHY/sources/strict.bash"
 
 if test "$#" -eq 1; then
 	outputFile="concat.$1"
-	if is-file "$outputFile"; then
+	if test -f "$outputFile"; then
 		rm "$outputFile"
 	fi
 

commands/get-app
@@ -31,7 +31,7 @@ appname="${name%.app}.app"
 # iterate through a few options
 for dir in "${dirs[@]}"; do
 	path="$dir/$appname"
-	if is-dir "$path"; then
+	if test -d "$path"; then
 		echo "$path"
 		exit 0
 	fi

commands/ios-dev
@@ -3,11 +3,11 @@ source "$DOROTHY/sources/strict.bash"
 
 # iOS Simulator
 app=$(get-app Xcode-beta.app)
-if is-dir "$app"; then
+if test -d "$app"; then
 	open "$app/Contents/Developer/Applications/Simulator.app"
 else
 	app=$(get-app Xcode.app)
-	if is-dir "$app"; then
+	if test -d "$app"; then
 		open "$app/Contents/Developer/Applications/Simulator.app"
 	else
 		echo "Xcode is not installed"

commands/is-missing
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-! test -e "$1"
+test ! -e "$1"

commands/itunes-owners
@@ -35,7 +35,7 @@ while read -r line; do
 	echo -n X
 	file="$(decode "$line")"
 	# echo "analysing: $file"
-	# if is-file "$file"; then
+	# if test -f "$file"; then
 	# 	echo "missing: $file"
 	# 	exit 1
 	# fi

commands/macos-state
@@ -32,7 +32,7 @@ if test "$action" = 'restore' -a -z "$backupRoot"; then
 		confirm "Press any key to continue..."
 		echo ''
 		backupRoot="$(tmutil latestbackup || exit 0)"
-		if ! is-dir "$backupRoot"; then
+		if test ! -d "$backupRoot"; then
 			echo ''
 			echo 'Unable to find the Time Machine backup automatically, attempting manual resolution...'
 			echo ''
@@ -43,7 +43,7 @@ if test "$action" = 'restore' -a -z "$backupRoot"; then
 			timeBackupMachine="$(choose-path "$timeBackupVolume/Backups.backupdb")"
 			backupRoot="$timeBackupMachine/Latest"
 		fi
-		if is-dir "$backupRoot"; then
+		if test -d "$backupRoot"; then
 			echo "=> $backupRoot"
 			backupType='tm'
 		else

commit a7e4036ad2d3d7107a22fcb452c77840f3e0684f

theme: fix oz theme on zsh
regression from https://github.com/bevry/dorothy/commit/5aec180b7a9e5329b712165f8f184c0a38d4c778

sources/theme.sh
@@ -5,7 +5,7 @@ if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
 		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			. "$DOROTHY/themes/oz"
 			export PROMPT_COMMAND="oztheme bash \$?"
-		elif test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
+		elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
 			. "$DOROTHY/themes/oz"
 			precmd() {
 				# shellcheck disable=SC3043