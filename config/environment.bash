a85a1212cbf81e7d020f1d70d49f81a811dc047c
Benjamin Lupton <b@lupton.cc>
2021-11-10 00:14:32 +0800

    setup-ruby: better homebrew ruby support
    On homebrew systems, there could by multiple ruby versions installed, as dependencies of other packages.
    If this is the case, this commit forces the user to select a preference.
    
    It also removes opt/llvm gem handling, not sure why that was there, perhaps a bug.
    And simplifies code around when `RUBY_VERSION` is not specified yet, allowing graceful operation.
    
    Compiler flags will only now be provided for the selecte `RUBY_VERSION` install.
    
    Also achieves formatting changes for `setup-ruby`, so progress on #74

--- config/environment.bash
+++ config/environment.bash
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
 # export NVM_DIR="$HOME/.nvm"
+# ^ used by: setup-environment-commands, setup-node
 
-# choose your architecture for apple silicon
 # export HOMEBREW_ARCH='x86_64' # 'arm64e'
+# ^ choose your architecture for apple silicon
+# ^ used by: setup-environment-commands, setup-mac-brew, brew
+
+# export PYENV_VERSION='3.10.0'  # only accepts full versions
+# ^ not currently used by: setup-environment-commands, setup-python
+
+# export RUBY_VERSION='default'
+# ^ used by: setup-environment-commands, setup-ruby
