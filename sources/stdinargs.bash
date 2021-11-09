5b6c078c1762ec9f92493ccb36ff04e597ba778a
Benjamin Lupton <b@lupton.cc>
2021-11-10 03:22:35 +0800

    fix vscode ssh not working, due to swallowed stdin
    The following issue:
    
    https://github.com/microsoft/vscode-remote-release/issues/5773
    
    Was being caused by `setup-environment-commands` calling `echo-if-executable` for setting up `yarn` and `go`, this would cause the `echo hello` in `echo 'echo hello' | ssh -v -o ConnectTimeout=15 -T -D 45689 raspi8.lan` to be swallowed by `echo-if-executable`'s sourcing of `stdinargs.bash`.
    
    Now `stdinargs.bash` only reads from stdin, if there are no arguments passed.
    
    Other changes:
    
    - `get-flag-value ... | echo-on-empty-stdin ...` has been replaced with `--missing=...` and if applicable `| echo-affirmative` calls
    - `fs-own` now documents its `--verbose` flag
    - `stdinargs.bash` will now error if `--help` was used but no `help` function was coded
    - `setup-environment-commands`:
        - correctly export `CARGO_TARGET_DIR`
        - no need anymore for the `echo-if-executable` usage after the `env.bash` usage, as `PATH` exists, so we can use `command-exists` instead

--- sources/stdinargs.bash
+++ sources/stdinargs.bash
@@ -2,36 +2,46 @@
 # calls the act function on each stdin line or argument
 
 # check for help argument
-if is-help "$@" && test "$(type -t help)" = 'function'; then
-	help && exit 22 # Invalid argument
-	exit "$?"
+if is-help "$@"; then
+	if test "$(type -t help)" = 'function'; then
+		help && exit 22 # Invalid argument
+		exit "$?"
+	else
+		echo "please update the function you called with a [help] method" >/dev/stderr
+		exit 38 # Function not implemented
+	fi
 fi
 
-# wait 0.1 seconds for stdin lines, and call act on each of them
-stdin="no"
-while IFS= read -rt 0.1 item; do
-	stdin="yes"
-	act "$item"
-done <&0
-
-# stdin was empty, so try arguments
-if test "$stdin" = "no"; then
-	# if no args, and noact exists, call noact
-	if test "$#" -eq 0; then
-		# check if noact exists
-		if test "$(type -t noact)" = 'function'; then
-			noact
-			exit "$?"
-		fi
-	fi
-
-	# we have args, so call act on each of them
+# attempt arguments first
+# arguments are instanous and won't mangle stdin for parent processes
+if test "$#" -ne 0; then
+	# we have args
+	# so call `act` on each of argument
 	for item in "$@"; do
 		act "$item"
 	done
+else
+	# args was empty, so attempt stdin
+	stdin='no'
+
+	# read stdin, with a timeout of 0.1 seconds
+	# if stdin works, then mark it as so
+	# and call `act` on each line
+	while IFS= read -rt 0.1 item; do
+		stdin='yes'
+		act "$item"
+	done </dev/stdin
+
+	# if stdin is empty
+	# then it means stdin and arguments were empty
+	# so if `noact` exists, call it
+	if test "$stdin" = 'no' -a "$(type -t noact)" = 'function'; then
+		noact
+		exit "$?"
+	fi
 fi
 
-# check if finish exists
+# if `finish` exists, call it
 if test "$(type -t finish)" = 'function'; then
 	finish
 	exit "$?"
