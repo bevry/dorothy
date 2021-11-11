8d7c1970fcfea2cf1640f200bef141f6df745afe
Benjamin Lupton <b@lupton.cc>
2021-11-11 23:34:03 +0800

    theme.sh, oz: zsh title finally works

--- sources/theme.sh
+++ sources/theme.sh
@@ -14,6 +14,11 @@ if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
 					echo 'DOROTHY has been moved, please re-open your shell'
 					return 1
 				fi
+				# export DISABLE_AUTO_TITLE="true"
+				# ^ @todo
+				# ^ apparently, with oh-my-zsh this is necessary,
+				# ^ however, I'm not a oh-my-zsh user, so I'm unsure if this is still necessary.
+				# ^ via standard zsh usage, it is not necessary.
 				oztheme zsh "$last_command_exit_status"
 			}
 		else
