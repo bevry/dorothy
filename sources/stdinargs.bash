109ccb2568fd21057eb37c3d9ecc7af110a0c734
Benjamin Lupton <b@lupton.cc>
2021-11-14 02:10:17 +0800

    rewrote `confirm*`, `setup-node`, `eval-collapse`
    user facing changes:
    
    - `confirm*` scripts have been reworked to:
        - support question details
        - output question with blink options, and upon finale, with its selection
        - support ctrl+c terminations, passing along exit code and indicating with ABORT
        - support spacebar for defaults, for items that support it
        - much more effecient too, no longer have to redo everything each invalid keypress
    - `setup-node` now cleans nvm versions, and if that fails, then cleans nvm, this can be prevented via NVM_CLEAN=no
    - `setup-node` also uses the new `eval-collapse` and `confirm` features for a cleaner experience
    - `setup-vim` now outputs successful replacements
    
    major internal changes:
    
    - added `echo-count-lines` to count lines
    - `echo-clear-lines` has been rejigged to support more edge cases, such as non-terminating lines, piped output, and erasing already outputted content
    - `eval-collapse` has been redone to be more reliable and easier to understand
    - `stdinargs` can now use `line` and `inline` and `arg` functions, this is used by `echo-count-lines`
    
    other changes:
    
    - `git-remote-apply` no longer does tty and stderr stuff, as it was interfering with `eval-collapse`
    - `setup-mac` cleaner success output

--- sources/stdinargs.bash
+++ sources/stdinargs.bash
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # calls the act function on each stdin line or argument
 
+export STDIN='no'
+
 # check for help argument
 if is-help "$@"; then
 	if test "$(type -t help)" = 'function'; then
@@ -18,24 +20,40 @@ if test "$#" -ne 0; then
 	# we have args
 	# so call `act` on each of argument
 	for item in "$@"; do
-		act "$item"
+		if test "$(type -t arg)" = 'function'; then
+			arg "$item"
+		else
+			act "$item"
+		fi
 	done
 else
 	# args was empty, so attempt stdin
-	stdin='no'
+	STDIN='no'
 
 	# read stdin, with a timeout of 0.1 seconds
 	# if stdin works, then mark it as so
 	# and call `act` on each line
 	while read -rt 0.1 item; do
-		stdin='yes'
-		act "$item"
+		STDIN='yes'
+		if test "$(type -t line)" = 'function'; then
+			line "$item"
+		else
+			act "$item"
+		fi
 	done </dev/stdin
+	if test -n "$item"; then
+		STDIN='yes'
+		if test "$(type -t inline)" = 'function'; then
+			inline "$item"
+		else
+			act "$item"
+		fi
+	fi
 
 	# if stdin is empty
 	# then it means stdin and arguments were empty
 	# so if `noact` exists, call it
-	if test "$stdin" = 'no' -a "$(type -t noact)" = 'function'; then
+	if test "$STDIN" = 'no' -a "$(type -t noact)" = 'function'; then
 		noact
 		exit "$?"
 	fi
