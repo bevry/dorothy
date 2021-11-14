29262a7f7dba3893554527da57238dc91f4567d9
Benjamin Lupton <b@lupton.cc>
2021-11-14 21:47:36 +0800

    `setup-mac-brew`: if using an outdated login shell, force upgrade
    Also prevents `bash -c` from loading dorothy via `init.sh` changes.
    
    And swaps `is-empty-string` with `test -z` in `ssh.sh`, as the former is unnecessary.

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+# WHY: test -z "${DOROTHY_LOADED-}"
+#
 # sometimes bash_profile and bashrc are loaded on new terminals
 # sometimes bash_profile is loaded at login, and bashrc is loaded on new terminals
 # sometimes bash_profile will remember and export variables, but not functions
@@ -8,7 +10,16 @@
 # ^ so can't check for functions, ruling out https://stackoverflow.com/a/14467452
 # so considering all that, define a normal (non global, non local) variable, and check for its existence
 # no bash v3 support: if test ! -v 'DOROTHY_LOADED'; then
-if test -z "${DOROTHY_LOADED-}"; then
+
+# WHY: "$0" != 'bash'
+#
+# `bash -c -- 'echo $BASH_VERSION'` was loading this script, when it shouldn't
+# only `bash -lc -- 'echo $BASH_VERSION'` should attempt to load this script
+# as otherwise we are loading aliases, ssh agents, and all the reset for scripts instead of login shell
+# as such, we need to detect this difference
+# "$0" will be prefixed with '-' if {zsh,bash} login shell, so if it is {bash,zsh} it is not a login shell and should be skipped
+
+if test -z "${DOROTHY_LOADED-}" -a "$0" != 'bash' -a "$0" != 'zsh'; then
 	# shellcheck disable=SC2034
 	DOROTHY_LOADED='yes'
 	# ^ do not export this, as that will interfere with the case where:
