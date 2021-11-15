6c7b7ac74d4a4e05e11e2f08aafba1bc205fc7fc
Benjamin Lupton <b@lupton.cc>
2021-11-15 21:37:54 +0800

    prevent theme inheritance in subshells
    also document the init issues more thoroughly

--- sources/theme.sh
+++ sources/theme.sh
@@ -4,7 +4,12 @@ if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
 	if test "$DOROTHY_THEME" = 'oz'; then
 		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
 			. "$DOROTHY/themes/oz"
-			export PROMPT_COMMAND="oztheme bash \$?"
+			PROMPT_COMMAND="oztheme bash \$?"
+			# ^ don't export PROMPT_COMMAND, as it will be inherited in subshells
+			# so: `select-shell bash`
+			# then open a new terminal
+			# then enter `bash -i`
+			# will error with `bash: oztheme: command not found`
 		elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
 			. "$DOROTHY/themes/oz"
 			precmd() {
@@ -22,7 +27,7 @@ if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
 				oztheme zsh "$last_command_exit_status"
 			}
 		else
-			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+			echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell" >/dev/stderr
 		fi
 	elif test "$DOROTHY_THEME" = 'starship'; then
 		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
@@ -31,19 +36,20 @@ if test -n "${DOROTHY_THEME-}" -a "${DOROTHY_THEME}" != 'system'; then
 			eval "$(starship init zsh)"
 		else
 			# https://starship.rs/guide/#🚀-installation
-			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+			echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell" >/dev/stderr
 		fi
 	elif test "$DOROTHY_THEME" = 'trial'; then
 		if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
-			export PROMPT_COMMAND="echo -n 'DorothyTrial> '"
+			PROMPT_COMMAND="echo -n 'DorothyTrial> '"
+			# ^ don't export PROMPT_COMMAND, as it will be inherited in subshells
 		elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
 			precmd() {
 				printf 'DorothyTrial> '
 			}
 		else
-			stderr echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell"
+			echo "dorothy does not yet support the theme [$DOROTHY_THEME] on this shell" >/dev/stderr
 		fi
 	else
-		stderr echo "dorothy does not understand the theme [$DOROTHY_THEME]"
+		echo "dorothy does not understand the theme [$DOROTHY_THEME]" >/dev/stderr
 	fi
 fi
