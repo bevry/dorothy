d68e71b99bed8ac49a756abda82ba1d480b711b6
Benjamin Lupton <b@lupton.cc>
2021-11-15 21:37:54 +0800

    prevent theme inheritance in subshells
    also document the init issues more thoroughly

--- init.sh
+++ init.sh
@@ -5,25 +5,59 @@
 # sometimes bash_profile and bashrc are loaded on new terminals
 # sometimes bash_profile is loaded at login, and bashrc is loaded on new terminals
 # sometimes bash_profile will remember and export variables, but not functions
-# ^ so can't check for exported variables, ruling export `export DOROTHY_LOADED=yes`
 # zsh loads zprofile, and zshrc, but forgets functions
-# ^ so can't check for functions, ruling out https://stackoverflow.com/a/14467452
-# so considering all that, define a normal (non global, non local) variable, and check for its existence
-# no bash v3 support: if test ! -v 'DOROTHY_LOADED'; then
+#
+# As such, we need a way that will prevent double loads, but prevent inheritance.
+# As functions in bash are sometimes forgotten, and functions in zsh are forgotten,
+#   that rules out https://stackoverflow.com/a/14467452 for this purpose.
+# And as exported variables will be inherited by subshells, that rules out exported variables.
+# As such, checking for the existence of a non-global non-local variable will work for this case.
+# Note that bash v3 does not support `test ! -v 'DOROTHY_LOADED'`, so have to use `test -z "${DOROTHY_LOADED-}"``
 
 # WHY: "$0" != 'bash'
 #
-# `bash -c -- 'echo $BASH_VERSION'` was loading this script, when it shouldn't
-# only `bash -lc -- 'echo $BASH_VERSION'` should attempt to load this script
-# as otherwise we are loading aliases, ssh agents, and all the reset for scripts instead of login shell
-# as such, we need to detect this difference
+# Sometimes, non-login shell invocations will attempt to load this script:
+# - `bash -c -- 'echo $BASH_VERSION'`
+# - `zsh -c -- 'echo $BASH_VERSION'`
 #
-# if actual login shell, then for bash $0="-bash", and for zsh $0="-zsh"
-# `bash -il` invocation will be $0="bash" however `shopt -qp login_shell` will return 0
-# `/bin/bash -il` invocation will be $0="/bin/bash" however `shopt -qp login_shell` will return 0
-# `/bin/zsh -il` invocation will be $0="/Users/balupton/.dorothy/init.sh" however [[ -o login ]] will return 0
+# Whereas only true login shells should, as well as manual login shell invocations:
+# - `bash -lc -- 'echo $BASH_VERSION'`
+# - `zsh -lc -- 'echo $BASH_VERSION'`
+#
+# As such, we need to detect this difference, here are the samples:
+#
+# A true bash login shell (v3):
+# - `$0` is `-bash`
+# - `$-` is `himBH` via this script and manully, or if v5 then `himBHs` manually
+# - `shopt -qp login_shell` returns `0`
+#
+# Manual invocation `bash -il` (v5):
+# - `$0` is `bash`
+# - `$-` is `himBH` via this script, then `himBHs` manually
+# - `shopt -qp login_shell` returns `0`
+#
+# Manual invocation `/bin/bash -il` (v3) and `/usr/local/bin/bash -il` (v5):
+# - `$0` is bash location: `/bin/bash`
+# - `$-` is "himBH" via this script and manually
+# - `shopt -qp login_shell` returns `0`
+#
+# A true zsh login shell:
+# - `$0` is `-zsh`
+# - `$-` is `569XZilm` via this script, then `569XZilms` manually
+# - `[[ -o login ]]` returns `0`
+#
+# Manual invocation of zsh login shell via `/bin/zsh -il`:
+# - `$0` is current script then zsh location
+# - `$-` is `569XZilm` via this script, then `569XZilms` manually
+# - `[[ -o login ]]` returns `0`
+#
+# Manual invocation of zsh non-login shell via `/bin/zsh -i`:
+# - `$0` is current script then zsh location
+# - `$-` is `569XZim` via this script, then `569XZims` manually
+# - `[[ -o login ]]` returns `0`
 
 # set -x # <debug>
+# printf '$0 = %s\n$- = %s\n' "$0" "$-"
 DOROTHY_LOAD='no'
 if test -z "${DOROTHY_LOADED-}"; then
 	if test "$0" = '-bash' -o "$0" = '-zsh'; then
