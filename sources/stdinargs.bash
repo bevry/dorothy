c5babaab84139d5b37df10aca7730435cbc36e55
Benjamin Lupton <b@lupton.cc>
2022-02-23 21:53:19 +0800

    contains-line: modern conventions, stdinargs redo
    stdinargs changes:
    
    - `noact` is now `no_args_nor_stdin`
    - `line` is fallback for `inline`
    - add `no_stdin` and `no_args`
    - cleaner code
    - `require_stdin` option
    
    These changes allow more robust usage, such as using args and stdin, in the case of `contains-lineq

--- sources/stdinargs.bash
+++ sources/stdinargs.bash
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-# calls the act function on each stdin line or argument
-
-export STDIN='no'
+source "$DOROTHY/sources/strict.bash"
 
 # allow sourcer to overide
+if test -z "${require_stdin-}"; then
+	require_stdin='no'
+fi
 if test -z "${timeout-}"; then
 	# don't use a value less than 1 (unless 0, which means infinite timeout)
 	# as too many commands take longer than a second to generate output (`mas search xcode` especially)
@@ -25,11 +26,17 @@ if test "${1-}" = '--help'; then
 	fi
 fi
 
+# prepare
+has_args='maybe'
+has_stdin='maybe'
+
 # attempt arguments first
-# arguments are instanous and won't mangle stdin for parent processes
-if test "$#" -ne 0; then
-	# we have args
-	# so call `act` on each of argument
+# arguments are instantanous and won't mangle stdin for parent processes
+if test "$#" -eq 0; then
+	has_args='no'
+else
+	# for each argument, call `arg` or `act`
+	has_args='yes'
 	for item in "$@"; do
 		if test "$(type -t arg)" = 'function'; then
 			arg "$item"
@@ -37,19 +44,22 @@ if test "$#" -ne 0; then
 			act "$item"
 		fi
 	done
-else
-	# args was empty, so attempt stdin
-	STDIN='no'
+fi
+
+# if no arguments, or stdin required, then do stdin
+if test "$has_args" != 'yes' -o "$require_stdin" = 'yes'; then
+	has_stdin='no'
 
 	# read stdin
 	# if stdin works, then mark it as so
-	# and call `act` on each line
+	# for each line, call `line` or `act`
+	# for each inline, call `inline` or `line` or `act`
 	read_args=()
 	if test -n "$timeout" -a "$timeout" -ne 0; then
 		read_args+=("-t" "$timeout")
 	fi
 	while read -r "${read_args[@]}" item; do
-		STDIN='yes'
+		has_stdin='yes'
 		if test "$(type -t line)" = 'function'; then
 			line "$item"
 		else
@@ -57,25 +67,33 @@ else
 		fi
 	done </dev/stdin
 	if test -n "$item"; then
-		STDIN='yes'
+		has_stdin='yes'
 		if test "$(type -t inline)" = 'function'; then
 			inline "$item"
+		elif test "$(type -t line)" = 'function'; then
+			line "$item"
 		else
 			act "$item"
 		fi
 	fi
+fi
 
-	# if stdin is empty
-	# then it means stdin and arguments were empty
-	# so if `noact` exists, call it
-	if test "$STDIN" = 'no' -a "$(type -t noact)" = 'function'; then
-		noact
-		exit "$?"
+# verify
+if test "$has_args" = 'no' -a "$has_stdin" = 'no'; then
+	if test "$(type -t no_args_nor_stdin)" = 'function'; then
+		no_args_nor_stdin
+	elif test "$(type -t noact)" = 'function'; then
+		noact # deprecated
 	fi
+fi
+if test "$has_args" = 'no' -a "$(type -t no_args)" = 'function'; then
+	no_args
+fi
+if test "$has_stdin" = 'no' -a "$(type -t no_stdin)" = 'function'; then
+	no_stdin
 fi
 
 # if `finish` exists, call it
 if test "$(type -t finish)" = 'function'; then
 	finish
-	exit "$?"
 fi
