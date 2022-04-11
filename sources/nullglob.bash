2dedbf9999086032baa090f5cf2d245454efe661
Benjamin Lupton <b@lupton.cc>
2022-04-12 00:48:29 +0800

    dorothy: fix nullglob failure on fresh installs

--- sources/nullglob.bash
+++ sources/nullglob.bash
@@ -3,6 +3,10 @@
 # https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 # nullglob: If set, Bash allows filename patterns which match no files to expand to a null string, rather than themselves.
 
+# https://github.com/bminor/bash/blob/master/CHANGES
+# nullglob is not referenced inside the changelog, so must be very old
+# and thus widely supported
+
 shopt -s nullglob || {
 	echo-style \
 		--error="Missing nullglob support." $'\n' \
