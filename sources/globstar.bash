cb10f3c19d3405a0e67f559f31c8ae8d6ca8d562
Benjamin Lupton <b@lupton.cc>
2022-04-14 10:37:33 +0800

    ci: debug https://github.com/bevry/dorothy/runs/6017707587

--- sources/globstar.bash
+++ sources/globstar.bash
@@ -3,6 +3,15 @@
 # https://www.gnu.org/software/bash/manual/html_node/The-Shopt-Builtin.html
 # globstar: If set, the pattern ‘**’ used in a filename expansion context will match all files and zero or more directories and subdirectories. If the pattern is followed by a ‘/’, only directories and subdirectories match.
 
+# Globstar came in bash v4, as such is not available on bash v3 that is provided by macOS
+# https://github.com/bminor/bash/blob/master/CHANGES
+# This document details the changes between this version, bash-4.0-alpha,
+# and the previous version, bash-3.2-release.
+#
+# w.  There is a new shell option: `globstar'.  When enabled, the globbing code
+#     treats `**' specially -- it matches all directories (and files within
+#     them, when appropriate) recursively.
+
 shopt -s globstar || {
 	echo-style \
 		--error="Missing globstar support." $'\n' \
