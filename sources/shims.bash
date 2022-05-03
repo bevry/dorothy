d8128ebe4645f2b134c05520e010567a37ddffd4
Benjamin Lupton <b@lupton.cc>
2022-05-04 02:07:44 +0800

    better bash v3 support
    - `eval-helper` uses bash v3 compatible syntax
    - `setup-util-bash` only upgrades bash if needed
    - `shims.bash` now links to more detailed bash version compat info

--- sources/shims.bash
+++ sources/shims.bash
@@ -1,25 +1,7 @@
 #!/usr/bin/env bash
 
 # https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
-
-# Bash v5.1 and up
-# dd. New `U', `u', and `L' parameter transformations to convert to uppercase,
-#     convert first character to uppercase, and convert to lowercase,
-#     respectively.
-# https://github.com/bminor/bash/blob/9439ce094c9aa7557a9d53ac7b412a23aa66e36b/CHANGES#L562-L564
-
-# Bash v4.2 and up
-# f.  test/[/[[ have a new -v variable unary operator, which returns success if
-#     `variable' has been set.
-# https://github.com/bminor/bash/blob/9439ce094c9aa7557a9d53ac7b412a23aa66e36b/CHANGES#L3642-L3643
-
-# Bash v4.0 and up
-# hh. There are new case-modifying word expansions: uppercase (^[^]) and
-#     lowercase (,[,]).  They can work on either the first character or
-#     array element, or globally.  They accept an optional shell pattern
-#     that determines which characters to modify.  There is an optionally-
-#     configured feature to include capitalization operators.
-# https://github.com/bminor/bash/blob/9439ce094c9aa7557a9d53ac7b412a23aa66e36b/CHANGES#L4710-L4714
+# Bash version compatibility: https://github.com/bevry/dorothy/discussions/151
 
 # If there is ever a double digit version part, we can change this, until then, this is perfect
 BASH_MAJOR_VERSION="${BASH_VERSION:0:1}"
