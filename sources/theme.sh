3afe13b18301da323f9c380add40b6b70ee353c8
Benjamin Lupton <b@lupton.cc>
2022-05-04 04:33:52 +0800

    better compat with fresh macos installs

--- sources/theme.sh
+++ sources/theme.sh
@@ -2,10 +2,8 @@
 
 if test -n "${DOROTHY_THEME-}" -a "$DOROTHY_THEME" != 'system'; then
 	if test -f "$DOROTHY/user/themes/${DOROTHY_THEME}.${ACTIVE_LOGIN_SHELL}"; then
-		# trunk-ignore(shellcheck/SC1090)
 		. "$DOROTHY/user/themes/${DOROTHY_THEME}.${ACTIVE_LOGIN_SHELL}"
 	elif test -f "$DOROTHY/themes/${DOROTHY_THEME}.${ACTIVE_LOGIN_SHELL}"; then
-		# trunk-ignore(shellcheck/SC1090)
 		. "$DOROTHY/themes/${DOROTHY_THEME}.${ACTIVE_LOGIN_SHELL}"
 	else
 		echo-style --notice="Dorothy theme [$DOROTHY_THEME] is not supported by this shell [$ACTIVE_LOGIN_SHELL]" >/dev/stderr
