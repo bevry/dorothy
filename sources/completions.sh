135ae7b894b3f56d4ebbdaaa0428c87dcd762306
Benjamin Lupton <b@lupton.cc>
2022-09-13 19:35:04 +0800

    renable bash completions
    70250469cb558f4170496faeaf5b0004236accb2 fixed them breaking fish
    
    also make bash and the completions quieter by default

--- sources/completions.sh
+++ sources/completions.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env sh
 
-# bash
-# if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
-# 	if test -f /etc/bash_completion; then
-# 		. '/etc/bash_completion'
-# 	elif test -n "${HOMEBREW_PREFIX-}" -a -f "${HOMEBREW_PREFIX-}/etc/bash_completion"; then
-# 		. "$HOMEBREW_PREFIX/etc/bash_completion"
-# 	fi
-# fi
+# Bash Completions
+if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
+	if test -f /etc/bash_completion; then
+		# trunk-ignore(shellcheck/SC1091)
+		. '/etc/bash_completion'
+	elif test -n "${HOMEBREW_PREFIX-}" -a -f "${HOMEBREW_PREFIX-}/etc/bash_completion"; then
+		# trunk-ignore(shellcheck/SC1091)
+		. "$HOMEBREW_PREFIX/etc/bash_completion"
+	fi
+fi
 
-# azure
+# 1Password
+# https://developer.1password.com/docs/cli/get-started#shell-completion
+if command-exists op; then
+	if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
+		eval "$(op completion bash)"
+	elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
+		eval "$(op completion zsh)"
+		compdef _op op
+	fi
+fi
+
+# Microsoft Azure
 if command-exists azure; then
 	eval '<(azure --completion)'
 fi
@@ -19,12 +32,6 @@ fi
 # brew cask install google-cloud-sdk
 # gcloud components install beta
 # gcloud init
-#
-# Firebase SDK
-# https://firebase.google.com/docs/functions/get-started
-# npm install -g firebase-tools
-# firebase init
-#
 if test -n "${HOMEBREW_PREFIX-}"; then
 	GDIR="${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk"
 	if test -d "$GDIR"; then
@@ -42,13 +49,7 @@ if test -n "${HOMEBREW_PREFIX-}"; then
 	fi
 fi
 
-# 1password
-# https://developer.1password.com/docs/cli/get-started#shell-completion
-if command-exists op; then
-	if test "$ACTIVE_LOGIN_SHELL" = 'bash'; then
-		eval "$(op completion bash)"
-	elif test "$ACTIVE_LOGIN_SHELL" = 'zsh'; then
-		eval "$(op completion zsh)"
-		compdef _op op
-	fi
-fi
+# Firebase SDK
+# https://firebase.google.com/docs/functions/get-started
+# npm install -g firebase-tools
+# firebase init
