bevry/dorothy

commit 6c18019a19644dfd442c711ef07d0f799f4cf329

remove `echo-command`, document alternatives in docs/coding/shells
roll out the correct alternative throughout dorothy

commands/mount-helper
@@ -968,14 +968,13 @@ function mount_helper() (
 		if test -n "$option_target"; then
 			# cron
 			if test -n "$option_share"; then
-				# using [which] as [command -v] fails on functions, which [mount-helper] is in this context
 				cron_args=(
 					# trunk-ignore(shellcheck/SC2230)
-					"$(which dorothy | echo-quote)"
+					"$(type -P dorothy | echo-quote)"
 					'run'
 					'--'
 					# trunk-ignore(shellcheck/SC2230)
-					"$(which mount-helper | echo-quote)"
+					"$(type -P mount-helper | echo-quote)"
 					'--mount'
 					'--no-remount'
 					"--type=${option_type@Q}"

sources/ssh.sh
@@ -23,7 +23,7 @@ if command-exists ssh-agent; then
 	# ensure ask pass is discoverable by the agent
 	if test -z "${SSH_ASKPASS-}" && command-exists ssh-askpass; then
 		export SSH_ASKPASS
-		SSH_ASKPASS="$(command -v ssh-askpass)"
+		SSH_ASKPASS="$(type -P ssh-askpass)"
 	fi
 	# setting [SSH_ASKPASS_REQUIRE] to [prefer] voids TTY responses
 

commands/command-exists
@@ -46,14 +46,6 @@ function command_exists() (
 	# =====================================
 	# Action
 
-	# fish shell:
-	# type --quiet "$command"
-
-	# bourne inspired shells:
-	# -P  force a PATH search for each NAME, even if it is an alias,
-	#     builtin, or function, and returns the name of the disk file
-	#     that would be executed
-
 	local command
 	for command in "${option_commands[@]}"; do
 		if type -P "$command" &>/dev/null; then

commands/config-helper
@@ -2,7 +2,7 @@
 
 function config_helper_test() (
 	source "$DOROTHY/sources/bash.bash"
-	source "$(command -v eval-tester)"
+	source "$(type -P eval-tester)"
 
 	# prepare
 	echo-segment --h1="TEST: $0"

commands/debug-bash
@@ -42,7 +42,7 @@ function debug_network() (
 	# =====================================
 	# Act
 
-	bash -xv "$(echo-command "${cmd[0]}")" "${cmd[@]:1}"
+	bash -xv "$(type -P "${cmd[0]}")" "${cmd[@]:1}"
 )
 
 # fire if invoked standalone

commands/dorothy
@@ -630,7 +630,7 @@ function dorothy() (
 	}
 
 	function ensure_permissions_configured {
-		source "$(command -v eval-helper)"
+		source "$(type -P eval-helper)"
 
 		eval_helper --quiet --no-wrap \
 			--pending="$(echo-style --bold="Correcting permissions...")" \
@@ -1399,7 +1399,7 @@ function dorothy() (
 
 		# able to test on bash v3?
 		local bash bashv3=''
-		bash="$(command -v bash)"
+		bash="$(type -P bash)"
 		if is_mac && test "$bash" != '/bin/bash' && [[ "$(/bin/bash --version || :)" == 'GNU bash, version 3.'* ]]; then
 			bashv3='/bin/bash'
 		fi

commands/down
@@ -2,7 +2,7 @@
 
 function down() (
 	source "$DOROTHY/sources/bash.bash"
-	source "$(command -v eval-helper)"
+	source "$(type -P eval-helper)"
 
 	# supported tools
 	local tool available_tools=() all_tools=(

commands/echo-command
@@ -1,43 +1,0 @@
-#!/usr/bin/env bash
-
-function echo_command() (
-	source "$DOROTHY/sources/bash.bash"
-
-	function help {
-		cat <<-EOF >/dev/stderr
-			ABOUT:
-			Output the path of the command.
-
-			USAGE:
-			echo-command <...input>
-			echo-lines <...input> | echo-command
-
-			EXAMPLE:
-
-			echo-command 'bash'
-
-			    /usr/bin/bash
-			    # exit status: 0
-
-			echo-lines 'bash' | echo-command
-
-			    /usr/bin/bash
-			    # exit status: 0
-
-		EOF
-		return 22 # EINVAL 22 Invalid argument
-	}
-
-	function on_input {
-		# these two options seem equivalent
-		command -v "$1"
-		# type -P "$1"
-	}
-
-	source "$DOROTHY/sources/stdinargs.bash"
-)
-
-# fire if invoked standalone
-if test "$0" = "${BASH_SOURCE[0]}"; then
-	echo_command "$@"
-fi

commit 714fea47503445820cf4eada450e8a25241fa6a8

fix linting regressions from 6c18019a19644dfd442c711ef07d0f799f4cf329

commands/mount-helper
@@ -969,11 +969,9 @@ function mount_helper() (
 			# cron
 			if test -n "$option_share"; then
 				cron_args=(
-					# trunk-ignore(shellcheck/SC2230)
 					"$(type -P dorothy | echo-quote)"
 					'run'
 					'--'
-					# trunk-ignore(shellcheck/SC2230)
 					"$(type -P mount-helper | echo-quote)"
 					'--mount'
 					'--no-remount'

sources/ssh.sh
@@ -23,7 +23,7 @@ if command-exists ssh-agent; then
 	# ensure ask pass is discoverable by the agent
 	if test -z "${SSH_ASKPASS-}" && command-exists ssh-askpass; then
 		export SSH_ASKPASS
-		SSH_ASKPASS="$(type -P ssh-askpass)"
+		SSH_ASKPASS="$(command -v ssh-askpass)"
 	fi
 	# setting [SSH_ASKPASS_REQUIRE] to [prefer] voids TTY responses
 