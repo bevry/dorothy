08066eee216a667fc949ca23c1c56106128adeaf
Benjamin Lupton <b@lupton.cc>
2023-09-17 05:37:41 +0800

    graduate `setup-server` from balupton/dotfiles
    `setup-server` has gone back and forth between dorothy core and my dotfiles...
    now that Dorothy core has `commands.beta` this back and forth can now end, and it can be graduated as a beta command, to indicate its WIP nature
    that said, `setup-server` has been powering several of my servers for several years now, and like the rest of doorthy, it has stablised.
    it can get promoted once it re-implements `--configure` support (it use to have it a year or so ago before the massive rewrites of this year)
    and once it has more users

---
+++ config/server.bash
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+# do not use `export` keyword in this file:
+# shellcheck disable=SC2034
+
+# Used by `setup-server`
+
+# The utilities (if any) to be installed and configured for this server:
+# local SHARE_UTILS=(
+# 	# 'gocryptfs'
+# 	# 'nordvpn'
+# 	# 'plex'
+# 	# 'samba'
+# 	# 'syncthing'
+# 	# 'transmission'
+# )
+
+# The username, group, and password for the user that will be created and associated with the services and mounts
+# local SHARE_USER='' SHARE_GROUP='' SHARE_PASSWORD=''
+
+# Optional [mount-helper] arguments for mounting the drive, vault, and samba shares (if any)
+# local DRIVE_MOUNT=() VAULT_MOUNT=() SAMBA_MOUNT=()
+
+# Optional customisation of where these services store their data:
+# local PLEX_HOME='' SYNCTHING_HOME='' TRANSMISSION_HOME=''
+
+# Optional NordVPN configuration:
+# local NORDVPN_TOKEN='' NORDVPN_DNS='' # 1.1.1.1 or 192.168.5.20 or whatever
