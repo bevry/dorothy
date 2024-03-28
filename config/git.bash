89c6a52a436b3a9d1aa2b496578b654b49ebe387
Benjamin Lupton <b@lupton.cc>
2024-03-28 17:20:48 +0800

    setup-system: when updating, less prompts
    setup-{git,ssh}, ssh-helper, {git,ssh}.bash:
    - fix 1password prompts not remembering, and move SSH_IDENTITY_AGENT from the extinct ssh.bash to the extant git.bash
    
    setup-{git,go,node,python,ruby,rust}:
    - implement an <action> and --configure combination that reduces prompts in an intuitive way
    
    /close #186

--- config/git.bash
+++ config/git.bash
@@ -2,10 +2,12 @@
 # do not use `export` keyword in this file:
 # shellcheck disable=SC2034
 
-# Used by `setup-git`
+# Used by `setup-git`, `ssh-helper`
 # You can use `setup-git --configure` to configure these.
 
 # GPG_SIGNING_KEY='' # use 'krypton' for Krypt.co
+# SSH_IDENTITY_AGENT='' # 'op' for 1password, 'krypton' for Krypt.co, 'system' for system, 'auto' for autodetect
+
 # GIT_DEFAULT_BRANCH='main'
 # GIT_PROTOCOL='' # 'https', or 'ssh'
 # GIT_NAME=''
