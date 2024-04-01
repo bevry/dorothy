bd6581a642aef9f2aabdb951cc0a63e8f3642862
Benjamin Lupton <b@lupton.cc>
2024-04-01 16:52:11 +0800

    setup-git: no need to prompt for 1password gpg key
    /close #186

--- config/git.bash
+++ config/git.bash
@@ -5,8 +5,9 @@
 # Used by `setup-git`, `ssh-helper`
 # You can use `setup-git --configure` to configure these.
 
-# GPG_SIGNING_KEY='' # use 'krypton' for Krypt.co
-# SSH_IDENTITY_AGENT='' # 'op' for 1password, 'krypton' for Krypt.co, 'system' for system, 'auto' for autodetect
+# GPG_SIGNING_KEY=''
+# GPG_SIGNING_AGENT=''    # 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
+# SSH_IDENTITY_AGENT=''   # 'op' for 1password, 'krypton' for Krypt.co, 'default' for system default
 
 # GIT_DEFAULT_BRANCH='main'
 # GIT_PROTOCOL='' # 'https', or 'ssh'
