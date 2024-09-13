d86e060a9d2792c13d765e4f45e30048b17ce9a8
Benjamin Lupton <b@lupton.cc>
2024-09-13 10:19:53 +0800

    WIP: move all deno scripts to .ts files, allow their discovery, prep for fixing this error: https://github.com/bevry/dorothy/actions/runs/10841571157/job/30085812259#step:2:774

---
+++ commands/brew-installed.ts
@@ -0,0 +1,39 @@
+// deno-lint-ignore-file no-explicit-any
+
+// parse stdin
+const json = await new Response(Deno.stdin.readable).json()
+
+// prepare result
+const names = new Set<string>()
+
+// add casks if not filtered to only formula
+if (Deno.args.includes('--formula') === false) {
+	// all casks are by request, so no need for special --requested handling for casks
+	for (const cask of json.casks.filter((i: any) => i.installed)) {
+		names.add(cask.full_token)
+	}
+}
+
+// add formulas if not filtered to only casks
+if (Deno.args.includes('--cask') === false) {
+	if (Deno.args.includes('--requested')) {
+		// only requested formula
+		for (const formula of json.formulae) {
+			if (formula.installed.find((i: any) => i.installed_on_request)) {
+				names.add(formula.full_name)
+			}
+		}
+	} else {
+		// all formula
+		for (const formula of json.formulae.filter((i: any) => i.installed)) {
+			names.add(formula.full_name)
+		}
+	}
+}
+
+// output result
+if (names.size) {
+	await Deno.stdout.write(
+		new TextEncoder().encode(Array.from(names).sort().concat('').join('\n')),
+	)
+}
