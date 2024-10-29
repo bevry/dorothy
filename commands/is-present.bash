c2f2ca90bd313b30d2132f6e6beeb9d5974f865f
Benjamin Lupton <b@lupton.cc>
2024-10-29 11:58:30 +0800

    move sudo calls to `[[` helpers
    - add `is-(directory|file|missing|not-directory|present|readable)(.bash)?`
    - is-missing: fix incorrect help text on exit status

---
+++ commands/is-present.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [[ $1 == '--' ]]; then
+	shift
+fi
+if [[ $# -eq 0 ]]; then
+	exit 22 # EINVAL 22 Invalid argument
+fi
+while [[ $# -ne 0 ]]; do
+	# just -e is faulty, as -e fails on broken symlinks
+	[[ -e $1 || -L $1 ]] || exit
+	shift
+done
+exit 0
---
+++ commands/is-readable.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ $1 == '--' ]]; then
+	shift
+fi
+if [[ $# -eq 0 ]]; then
+	exit 22 # EINVAL 22 Invalid argument
+fi
+while [[ $# -ne 0 ]]; do
+	[[ -r $1 ]] || exit
+	shift
+done
+exit 0
