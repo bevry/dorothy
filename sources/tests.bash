b073dec27abca24e4bb8b2433ef699967e296fff
Benjamin Lupton <b@lupton.cc>
2025-01-08 17:30:26 +0800

    fs-own, tests: more attempts to resolve root tests weirdness

--- sources/tests.bash
+++ sources/tests.bash
@@ -117,7 +117,7 @@ function fs_tests__prep {
 		"$root/targets/unwritable-empty-file" \
 		"$root/targets/unwritable-filled-dir" \
 		"$root/targets/unwritable-filled-file"
-	sudo-helper -- chown 'root:root' \
+	sudo-helper -- chown '0:0' \
 		"$root/targets/unaccessible-empty-dir" \
 		"$root/targets/unaccessible-empty-file" \
 		"$root/targets/unaccessible-filled-dir" \
