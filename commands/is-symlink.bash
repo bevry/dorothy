cf7eceb87e93269ca872f51874ade737aa6621dc
Benjamin Lupton <b@lupton.cc>
2025-01-17 23:16:21 +0800

    is-present, is-symlink: new fs test conventions

--- commands/is-symlink.bash
+++ commands/is-symlink.bash
@@ -17,12 +17,12 @@ while [[ $# -ne 0 ]]; do
 	if [[ -L $path ]]; then
 		# is a symlink (broken or otherwise, accessible or otherwise)
 		continue
-	elif [[ -e $1 ]]; then
+	elif [[ -e $path ]]; then
 		# does exist: but not a symlink
 		exit 79 # EFTYPE 79 Inappropriate file type or format
 	else
 		# discern if inaccessible, missing
-		is-accessible.bash -- "$path" || exit $?
+		is-accessible.bash -- "$path" || exit
 		exit 2 # ENOENT 2 No such file or directory
 	fi
 done
