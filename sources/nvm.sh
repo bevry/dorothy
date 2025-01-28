0a99c0ac5b46202fc99e5f296f64887e600e5939
Benjamin Lupton <b@lupton.cc>
2025-01-28 23:57:28 +0800

    nvm.sh: fix continued failures in shell that called `setup-node`
    ```
    > nvm-env -- npm
    ++ '[' -z /Users/balupton/.nvm ']'
    ++ '[' -s /Users/balupton/.nvm/nvm.sh ']'
    ++ . /Users/balupton/.nvm/nvm.sh
    +++ NVM_SCRIPT_SOURCE=']'
    +++ '[' -z '' ']'
    +++ export NVM_CD_FLAGS=
    +++ NVM_CD_FLAGS=
    +++ nvm_is_zsh
    +++ '[' -n '' ']'
    +++ '[' -z /Users/balupton/.nvm ']'
    +++ case $NVM_DIR in
    +++ unset NVM_SCRIPT_SOURCE
    +++ nvm_process_parameters
    +++ local NVM_AUTO_MODE
    +++ NVM_AUTO_MODE=use
    +++ '[' 0 -ne 0 ']'
    +++ nvm_auto use
    +++ local NVM_MODE
    +++ NVM_MODE=use
    +++ case "${NVM_MODE}" in
    +++ local VERSION
    +++ local NVM_CURRENT
    ++++ nvm_ls_current
    ++++ local NVM_LS_CURRENT_NODE_PATH
    +++++ command which node
    ++++ NVM_LS_CURRENT_NODE_PATH=
    ++++ nvm_echo none
    ++++ command printf '%s\n' none
    +++ NVM_CURRENT=none
    +++ '[' _none = _none ']'
    ++++ nvm_resolve_local_alias default
    ++++ nvm_echo
    ++++ command printf '%s\n' ''
    +++ VERSION=N/A
    +++ '[' -n N/A ']'
    +++ '[' _N/A '!=' _N/A ']'
    +++ return 0
    + npm
    env: node: No such file or directory
    ```

--- sources/nvm.sh
+++ sources/nvm.sh
@@ -4,6 +4,10 @@
 # check that:
 # that NVM_DIR possibly exists
 # that nvm.sh is a non-empty file
+if ! command which node >/dev/null 2>&1; then
+	# workaround that can happen when the node version changes in the host shell, and nvm is no longer able to detect it, as [command which node] is what it uses
+	. "$DOROTHY/sources/environment.sh"
+fi
 if [ -z "${NVM_DIR-}" ]; then
 	export NVM_DIR="$HOME/.nvm"
 fi
