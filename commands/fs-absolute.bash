2b7f070b1cc5455e450c91628b8eb1b00f136c4e
Benjamin Lupton <b@lupton.cc>
2025-04-17 13:15:45 +0800

    continuation of auto-elevation of `fs-*` commands
    adds tests for `video-merge`, fixes tests for `fs-path`, and preps merge of `fs-realpath` and `fs-absolute` into `fs-path`, while expanding abilities of path resolution tooling

--- commands/fs-absolute.bash
+++ commands/fs-absolute.bash
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+option_resolve='no'
+if [[ $1 == '--resolve' ]]; then
+	option_resolve='yes'
+	shift
+fi
 if [[ $1 == '--' ]]; then
 	shift
 fi
@@ -13,36 +18,50 @@ while [[ $# -ne 0 ]]; do
 	path="$1"
 	shift
 
-	# Affirm accessibility
-	if [[ ! -e $path ]]; then
-		# discern if inaccessible
-		is-accessible.bash -- "$path" || exit
-		# if missing, then it only matters if the parent is missing, which the following will deal with
+	# if desired, resolve symlinks
+	if [[ $option_resolve == 'yes' ]]; then
+		if is-directory.bash -- "$path"; then
+			(
+				cd "$path"
+				pwd -P
+			)
+			continue
+		elif is-not-symlink.bash -- "$path"; then
+			filename="$(basename -- "$path")"
+			(
+				cd "$(dirname -- "$path")"
+				printf '%s\n' "$(pwd -P)/$filename"
+			)
+			continue
+		else
+			exit 45 # ENOTSUP 45 Operation not supported
+		fi
 	fi
 
-	# It is accessible and exists
-	# don't use [pwd -P] as -P resolves symlinks
-	# and resolving symlinks is what [fs-realpath] is for
+	# don't resolve symlinks
 	filename="$(basename -- "$path")"
+	dirname="$(dirname -- "$path")"
+	is-accessible.bash -- "$dirname" || exit # not accessible, supported, but needs elevation
+	is-present.bash -- "$dirname" || exit 45 # ENOTSUP 45 Operation not supported
 	if [[ $filename == '/' ]]; then
 		# handles root
 		printf '%s\n' '/'
 	elif [[ $filename == '..' ]]; then
 		# handles parent
 		(
-			cd "$(dirname -- "$path")/.."
+			cd "$dirname/.." || exit # unknown failure, should have been caught earlier
 			pwd
 		)
 	elif [[ $filename == '.' ]]; then
 		# handles cwd
 		(
-			cd "$(dirname -- "$path")"
+			cd "$dirname" || exit # unknown failure, should have been caught earlier
 			pwd
 		)
 	else
 		# handles files and directories
 		(
-			cd "$(dirname -- "$path")"
+			cd "$dirname" || exit # unknown failure, should have been caught earlier
 			printf '%s\n' "$(pwd)/$filename"
 		)
 	fi
