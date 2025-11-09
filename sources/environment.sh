d74905d0e448523da32933f4e247118f6e011a58
Benjamin Lupton <b@lupton.cc>
2025-11-09 19:47:05 +0800

    env: better bash failure messaging

--- sources/environment.sh
+++ sources/environment.sh
@@ -15,16 +15,18 @@ else
 	ACTIVE_POSIX_SHELL='sh'
 fi
 
-# set the environment variables, passing over $$ such that a trap could be hooked that automatically refreshes on invalidation (not yet implemented)
-DOROTHY_ENVIRONMENT_EVAL_STATUS=0
-DOROTHY_ENVIRONMENT_EVAL="$("$DOROTHY/commands/setup-environment-commands" --shell="$ACTIVE_POSIX_SHELL" --ppid=$$)" || DOROTHY_ENVIRONMENT_EVAL_STATUS=$?
-if [ "$DOROTHY_ENVIRONMENT_EVAL_STATUS" = 0 ]; then
-	eval "$DOROTHY_ENVIRONMENT_EVAL" || DOROTHY_ENVIRONMENT_EVAL_STATUS=$?
-fi
-if [ "$DOROTHY_ENVIRONMENT_EVAL_STATUS" != 0 ]; then
-	printf '%s\n' 'DOROTHY FAILED TO SETUP ENVIRONMENT, RUN THIS TO DEBUG:' "bash -x '$DOROTHY/commands/setup-environment-commands' --shell='$ACTIVE_POSIX_SHELL'" >&2 || :
-	if [ -n "${CI-}" ]; then
-		bash -x "$DOROTHY/commands/setup-environment-commands" --shell="$ACTIVE_POSIX_SHELL" >&2 || :
-	fi
-	return "$DOROTHY_ENVIRONMENT_EVAL_STATUS"
+# create the environment setup, passing over $$ such that a trap could be hooked that automatically refreshes on invalidation (not yet implemented)
+DOROTHY_ENVIRONMENT_EVAL="$("$DOROTHY/commands/setup-environment-commands" --shell="$ACTIVE_POSIX_SHELL" --ppid=$$)" || {
+	printf '%s\n' \
+    'DOROTHY FAILED TO CREATE ENVIRONMENT SETUP, RUN THESE TO DEBUG:' \
+    "'$DOROTHY/commands/setup-environment-commands' --debug --shell='$ACTIVE_POSIX_SHELL'" \
+    "bash -x '$DOROTHY/commands/setup-environment-commands' --shell='$ACTIVE_POSIX_SHELL'" >&2 || :
+}
+# evaluate the environment setup
+if [ -n "${DOROTHY_ENVIRONMENT_EVAL-}" ]; then
+	eval "$DOROTHY_ENVIRONMENT_EVAL" || {
+    printf '%s\n' "\
+      EVALUATION OF THIS DOROTHY ENVIRONMENT SETUP WITH $?:" >&2 || :
+    cat -vbn <<<"$DOROTHY_ENVIRONMENT_EVAL" >&2 || :
+  }
 fi
